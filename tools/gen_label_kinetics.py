43b701e19234bc00b76fbf52931d14fc747ddde6
Zhenyang <lizhenyang6@jd.com>
2019-10-31 16:28:35 +0800

    update

--- tools/gen_label_kinetics.py
+++ tools/gen_label_kinetics.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     with open('kinetics_label_map.txt') as f:
         categories = f.readlines()
         #categories = [c.strip().replace(' ', '_').replace('"', '').replace('(', '').replace(')', '').replace("'", '') for c in categories]
-        categories = [c.strip().replace(' ', '_') for c in categories]
+        categories = [c.strip().replace(' ', '_').replace('"', '') for c in categories]
     assert len(set(categories)) == 400
     dict_categories = {}
     for i, category in enumerate(categories):
@@ -42,7 +42,7 @@ if __name__ == '__main__':
             #folders.append(items[1] + '_' + items[2])
             folders.append( "%s_%06d_%06d" % (items[1], int(items[2]), int(items[3])) )
             #this_catergory = items[0].replace(' ', '_').replace('"', '').replace('(', '').replace(')', '').replace("'", '')
-            this_catergory = items[0].replace(' ', '_')
+            this_catergory = items[0].replace(' ', '_').replace('"', '')
             categories_list.append(this_catergory)
             idx_categories.append(dict_categories[this_catergory])
             count_cat[this_catergory] += 1
