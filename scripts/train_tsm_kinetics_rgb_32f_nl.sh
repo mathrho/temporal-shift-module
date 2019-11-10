9dd7dbf85422190ec86af86f84ec9de6446a5846
Zhenyang <mathrho@vpn-staff-146-50-68-115.vpn.uva.nl>
2019-11-10 14:32:01 +0800

    update train conf

--- scripts/train_tsm_kinetics_rgb_32f_nl.sh
+++ scripts/train_tsm_kinetics_rgb_32f_nl.sh
@@ -2,6 +2,6 @@
 python main.py kinetics RGB \
      --gpus 0 1 2 3 \
      --arch resnet50 --num_segments 32 \
-     --gd 20 --lr 0.04 --wd 1e-4 --lr_steps 20 40 --epochs 50 \
-     --batch-size 256 -j 16 --dropout 0.5 --consensus_type=avg --eval-freq=1 \
+     --gd 20 --lr 0.02 --wd 1e-4 --lr_steps 20 40 --epochs 50 \
+     --batch-size 128 -j 16 --dropout 0.5 --consensus_type=avg --eval-freq=1 \
      --shift --shift_div=8 --shift_place=blockres --non_local --npb
