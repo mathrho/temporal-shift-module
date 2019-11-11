c1c5ce87858965af653f8b49284820cd4e3beab0
Zhenyang <lizhenyang6@jd.com>
2019-11-11 11:25:55 +0800

    update

--- scripts/train_tsm_kinetics_rgb_16f_nl.sh
+++ scripts/train_tsm_kinetics_rgb_16f_nl.sh
@@ -3,4 +3,4 @@ python main.py kinetics RGB \
      --arch resnet50 --num_segments 16 \
      --gd 20 --lr 0.0025 --wd 1e-4 --lr_steps 20 40 --epochs 50 \
      --batch-size 16 -j 16 --dropout 0.5 --consensus_type=avg --eval-freq=1 \
-     --shift --shift_div=8 --shift_place=blockres --non_local
+     --shift --shift_div=8 --shift_place=blockres --non_local  --npb
